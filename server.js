210c27a04a2d88239586aea9d191da42885f4ece
ericpkatz <ericpkatz@gmail.com>
2023-02-27 19:17:11 -0500

    api is built

---
+++ server.js
@@ -0,0 +1,61 @@
+const Sequelize = require('sequelize');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_things_spa_db');
+
+const Thing = conn.define('thing', {
+  name: {
+    type: Sequelize.STRING
+  },
+  description: {
+    type: Sequelize.TEXT
+  }
+});
+
+Thing.addHook('beforeSave', (thing)=> {
+  if(!thing.description){
+    thing.description = new Array(50).fill(thing.name).join('\n');
+  }
+});
+
+const express = require('express');
+const app = express();
+
+app.get('/api/things', async(req, res, next)=> {
+  try {
+    res.send(await Thing.findAll({
+      attributes: {
+        exclude: ['description']
+      }
+    }));
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.get('/api/things/:id', async(req, res, next)=> {
+  try {
+    res.send(await Thing.findByPk(req.params.id));
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, async()=> {
+  try {
+    console.log(`listening on port ${port}`);
+    await conn.sync({ force: true });
+    const [foo, bar, bazz, quq] = await Promise.all(['foo', 'bar', 'bazz', 'quq'].map( name => {
+      return Thing.create({ name }); 
+    }));
+    await quq.update({
+      description: 'QUQ!!!!'
+    });
+    console.log('ready to run');
+  }
+  catch(ex){
+    console.log(ex);
+  }
+});
